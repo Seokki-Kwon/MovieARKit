e82ed7ad7e52ec8480025c399f41ccdcdf60bea0
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-01-11 17:54:52 +0900

    add overlay

---
+++ MovieARKit/Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extensions.swift
+//  MovieARKit
+//
+//  Created by 권석기 on 1/11/25.
+//
+
+import Foundation
+
+extension Int {
+    var degreesToRadinans: Double { return Double(self) * .pi/180}
+}
--- MovieARKit/ViewController.swift
+++ MovieARKit/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet weak var addButton: UIButton!
@@ -22,10 +22,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         addNotification()
     }
     
+    func createFloor(anchor: ARPlaneAnchor) -> SCNNode {
+        let floor = SCNNode()
+        floor.name = "Floor"
+        floor.eulerAngles = SCNVector3(90.degreesToRadinans, 0, 0)
+        floor.geometry = SCNPlane(width: CGFloat(anchor.planeExtent.width), height: CGFloat(anchor.planeExtent.height))
+        
+        floor.geometry?.firstMaterial?.diffuse.contents = UIColor.blue.withAlphaComponent(0.4)
+        floor.geometry?.firstMaterial?.isDoubleSided = true
+        floor.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
+        return floor
+    }
+    
+    func removeNode(named: String) {
+        sceneView.scene.rootNode.enumerateChildNodes { node, _ in
+            if node.name == "Floor" {
+                node.removeFromParentNode()
+            }
+        }
+    }
+    
     func setupScene() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
         sceneView.addGestureRecognizer(tapGesture)
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         sceneView.delegate = self
+        sceneView.session.delegate = self
         let scene = SCNScene()
         sceneView.scene = scene
     }
@@ -60,7 +82,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Run the view's session
         configuration.planeDetection = .horizontal
-        sceneView.session.run(configuration)
+        sceneView.session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -85,5 +107,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let scene = SCNScene(named: filename)
         let node = scene?.rootNode.childNode(withName: "Geom", recursively: true)
         currentNode = node
+    }
+}
+
+extension ViewController {
+    func renderer(_ renderer: any SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        // 장면에 새로운 ARAnchor 노드가 추가될때 호출
+        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
+        let floorNode = createFloor(anchor: anchorPlane)
+        node.addChildNode(floorNode)
+    }
+    
+    func renderer(_ renderer: any SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
+        let floorNode = createFloor(anchor: anchorPlane)
+        //        node.removeFromParentNode()
+        node.addChildNode(floorNode)
+    }
+    
+    func renderer(_ renderer: any SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        print("Remove node")
+        removeNode(named: "Floor")
     }
 }
