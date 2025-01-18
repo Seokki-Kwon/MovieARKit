2f4457ab7f553945916dc9889bb1646dcf7f7406
Seokki-Kwon <rnjstjrrl12@gmail.com>
2025-01-18 18:05:35 +0900

    resize focusWidth

--- MovieARKit/ViewController+Focus.swift
+++ MovieARKit/ViewController+Focus.swift
@@ -13,16 +13,31 @@ extension ViewController {
         if let camera = sceneView.session.currentFrame?.camera, case .normal = camera.trackingState,
            let query = self.getRaycastQuery(from: sceneView.center),
            let result = sceneView.session.raycast(query).first {
+            // 포커스 노드가 존재하는경우 위치업데이트
             if let pointerNode = sceneView.scene.rootNode.childNode(withName: "pointer", recursively: true) {
                 pointerNode.simdTransform = result.worldTransform
+                resizeFocus(for: pointerNode, camera: camera)
             } else {
-                let plane = SCNBox(width: 0.1, height: 0.01, length: 0.1, chamferRadius: 0)
-                plane.firstMaterial?.diffuse.contents = UIColor.green.withAlphaComponent(0.8)
+                // 포커스노드 생성
+                let plane = SCNBox(width: 0.2, height: 0.001, length: 0.2, chamferRadius: 0)
+                plane.firstMaterial?.diffuse.contents = UIImage(named: "focus")
                 let node = SCNNode(geometry: plane)
                 node.name = "pointer"
                 node.simdTransform = result.worldTransform
                 sceneView.scene.rootNode.addChildNode(node)
             }
         }
+    }
+    
+    func resizeFocus(for node: SCNNode, camera: ARCamera) {
+        guard let planeGeometry = node.geometry as? SCNBox else { return }
+        let cameraPosition = simd_make_float3(camera.transform.columns.3)
+        let nodePosition = simd_make_float3(node.simdTransform.columns.3)
+        let distance = simd_length(cameraPosition - nodePosition)
+        // 거리가 늘어난다 -> 크기 확대
+        // 거리가 줄어든다 -> 크기 축소
+        let scaledWidth = CGFloat(0.2 * distance)
+        planeGeometry.width = scaledWidth
+        planeGeometry.length = scaledWidth
     }
 }
